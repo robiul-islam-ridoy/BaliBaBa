f87b9e99c7349381b987449cf5dd7d646f147ea7
Robiul Islam <robiulislamhr.bd@gmail.com>
2025-09-13 23:45:50 +0600

    unnecessary file removed

--- public/js/config.js
+++ public/js/config.js
@@ -3,12 +3,3 @@ export async function getFirebaseConfig() {
   if (!res.ok) throw new Error('Failed to load Firebase config');
   return await res.json();
 }
-
-// export const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID
-// };
